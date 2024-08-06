275b799aee0398d7d5465236904d078b1865b8b5
3380Student891 <goyala4@student.douglascollege.ca>
2024-08-06 10:36:22 -0700

    Updated AddItem.jsx for Render

--- frontend/src/components/AddItem.jsx
+++ frontend/src/components/AddItem.jsx
@@ -32,7 +32,7 @@ function AddItem() {
           }
       
           try {
-            const response = await axios.post('http://localhost:5001/api/additem', formData, {
+            const response = await axios.post('https://three380g5project.onrender.com/api/additem', formData, {
               headers: {
                 'Content-Type': 'multipart/form-data'
               }
