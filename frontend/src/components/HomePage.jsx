868451c74b12fd4d3be7745671a04c3c6cc0b1b6
3380Student891 <goyala4@student.douglascollege.ca>
2024-08-06 09:31:51 -0700

    Updated HomePage.jsx for Render

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -13,6 +13,7 @@ function HomePage() {
   const [timers, setTimers] = useState({});
   const [searchKeyword, setSearchKeyword] = useState('');
   const navigate = useNavigate();
+  const apiUrl = "https://three380g5project.onrender.com";
 
   const addItem = (e) => {
     e.preventDefault();
@@ -29,7 +30,7 @@ function HomePage() {
 
   useEffect(() => {
     // Fetch items from backend API using axios
-    axios.get('https://three380g5project.onrender.com/api/getitems')
+    axios.get(`${apiUrl}/api/getitems`)
       .then(response => {
         console.log('Fetched items:', response.data);
         setItems(response.data);
@@ -87,7 +88,7 @@ function HomePage() {
   const searchItems = useCallback(
     debounce((keyword) => {
       if (keyword.trim() === '') {
-        axios.get('/api/getitems')
+        axios.get(`${apiUrl}/api/getitems`)
           .then(response => {
             setItems(response.data);
           })
@@ -95,7 +96,7 @@ function HomePage() {
             console.error('Error fetching items:', error);
           });
       } else {
-        axios.get(`/api/searchitems?keyword=${keyword}`)
+        axios.get(`${apiUrl}/api/searchitems?keyword=${keyword}`)
           .then(response => {
             setItems(response.data);
           })
@@ -136,7 +137,7 @@ function HomePage() {
 const ItemCard = ({ item, updateItemBid, remainingTime }) => {
   return (
     <div className="card">
-      <img src={`/api/image/${item._id}`} alt={item.itemName} />
+      <img src={`${apiUrl}/api/image/${item._id}`} alt={item.itemName} />
       <h2>{item.itemName}</h2>
       <p>{item.itemDescription}</p>
       <p>Starting Price: ${item.itemStartingPrice}</p>
@@ -168,7 +169,7 @@ const UpdateBid = ({item, updateItemBid}) => {
       itemBidPrice: Number(item.itemBidPrice)+Number(bid),
       buyerName: buyerName
   };
-    axios.put(`http://localhost:5001/api/updatebid/${item._id}`, newBid)
+    axios.put(`${apiUrl}/api/api/updatebid/${item._id}`, newBid)
       .then(response => {
         console.log('Bid updated:', response.data);
         Swal.fire({
